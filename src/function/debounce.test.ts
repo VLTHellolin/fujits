f7d6899146fb23ac19380cbfde3745fee6c77324
VLTHellolin <i@hellolin.top>
2025-10-11 20:41:33 +0800

    test(function): use vitest spy functions

--- src/function/debounce.test.ts
+++ src/function/debounce.test.ts
@@ -1,82 +1,70 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { debounce } from './debounce';
 
 describe('debounce', () => {
   it('should debounce a function with trailing invocation', async () => {
-    let callCount = 0;
-    const debouncedFunc = debounce(() => {
-      callCount++;
-    }, 100);
+    const spy = vi.fn();
+    const func = debounce(spy, 100);
 
-    debouncedFunc();
-    debouncedFunc();
-    debouncedFunc();
+    func();
+    func();
+    func();
 
-    expect(callCount).toBe(0);
-
+    expect(spy).toBeCalledTimes(0);
     await new Promise(r => setTimeout(r, 150));
-    expect(callCount).toBe(1);
+    expect(spy).toBeCalledTimes(1);
   });
 
   it('should debounce a function with leading invocation', async () => {
-    let callCount = 0;
-    const debouncedFunc = debounce(
-      () => {
-        callCount++;
-      },
+    const spy = vi.fn();
+    const func = debounce(
+      spy,
       100,
       { invokeAt: 'leading' },
     );
 
-    debouncedFunc();
-    debouncedFunc();
-    debouncedFunc();
+    func();
+    func();
+    func();
 
-    expect(callCount).toBe(1);
-
+    expect(spy).toBeCalledTimes(1);
     await new Promise(r => setTimeout(r, 150));
-    expect(callCount).toBe(1);
+    expect(spy).toBeCalledTimes(1);
   });
 
   it('should debounce a function with both leading and trailing invocation', async () => {
-    let callCount = 0;
-    const debouncedFunc = debounce(
-      () => {
-        callCount++;
-      },
+    const spy = vi.fn();
+    const func = debounce(
+      spy,
       100,
       { invokeAt: 'both' },
     );
 
-    debouncedFunc();
-    debouncedFunc();
-    debouncedFunc();
+    func();
+    func();
+    func();
 
-    expect(callCount).toBe(1);
-
+    expect(spy).toBeCalledTimes(1);
     await new Promise(r => setTimeout(r, 150));
-    expect(callCount).toBe(2);
+    expect(spy).toBeCalledTimes(2);
   });
 
   it('should not invoke the function if aborted before delay', async () => {
-    let callCount = 0;
+    const spy = vi.fn();
     const controller = new AbortController();
-    const debouncedFunc = debounce(
-      () => {
-        callCount++;
-      },
+    const func = debounce(
+      spy,
       100,
       { signal: controller.signal },
     );
 
-    debouncedFunc();
-    debouncedFunc();
+    func();
+    func();
     controller.abort();
-    debouncedFunc();
+    func();
 
-    expect(callCount).toBe(0);
-
+    expect(spy).toBeCalledTimes(0);
     await new Promise(r => setTimeout(r, 150));
-    expect(callCount).toBe(0);
+    expect(spy).toBeCalledTimes(0);
   });
 });
